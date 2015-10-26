ad91a19d29cedb26647dbedabb7998a785e21d2c
jackyon <jackyon@jackyondeMacBook-Pro.local>
2015-10-26 14:37:49 +0800

    updates

---
+++ README.md
@@ -0,0 +1,85 @@
+<h1>Webpack base structure with reactjs</h1>
+> Containing modern web development tools such as Webpack, React Hot Loader, Babel, sass, autoprefixer, BrowserSync, Tinypng...
+
+Directory Layout:
+```
+├── /app/                       # The source code of the application
+│   ├── /assets/            	# public js/images/fonts
+│   ├── /components/            # components/pages
+│   ├── /tmpl/               	# Static content (plain HTML or Markdown)
+│
+├── /build/                     # The folder for compiled output
+├── /dist/                      # The folder for deploy output
+├── /node_modules/              # 3rd-party libraries and utilities
+├── webpack.config.js           # configuration file for webpack
+├── package.json                # The list of 3rd party libraries and utilities
+```
+
+<h2>Packages Install</h2>
+```
+$ npm install
+```
+
+<h2>Usage</h2>
+
+Dev:
+```
+$ npm run dev
+```
+Browser sync:
+```
+$ npm run browsersync
+```
+Deploy:
+```
+$ npm run deploy
+```
+Image compress:<br>
+<h5>PNG:</h5>
+```
+$ npm run tinypng
+```
+> hint: png compress is using tinypng service, make sure you change the above api to your owns. 
+> Get api: https://tinypng.com/developers (free 500 images/month. )
+> after you get the api, run:
+> $ tinypng -k I_KC7xGPxXfZPrEbrc-kXWBetAQ323rz 
+
+<h5>JPEG:</h5>
+```
+fot now, I'm still using jpegmini, this is the best tools to compress the jpeg images that I discovered so far.
+the jpegmini app is quite simple to optimize the photos, just simple drag. But I will consider to integrate this super tool into webpack. Still researching...
+```
+
+
+To do:
+<ol>
+	<li>Thinking a smart way to deploy the files to remote server without using gulp/grunt.</li>
+	<li>More usful plugins will include in...</li>
+</ol>
+
+<h2>Hints:</h2>
+<ol>
+	<li>
+		why not using webpack imagein plugin, cause that plugin doesn't save so much sizes. if you have better solution plz contact me.
+
+		By default " npm run tinypng" will replace the original png files, there more useage:
+		```
+		tinypng [options] [image.png|*.png]
+		  -k, --api-key       Set default TinyPNG API key.
+		  -r, --allow-rewrite Rewrite the original files with compressed data.
+		  -n, --allow-nonpng  Allow you to compress files without .png extention.
+		  -p, --postfix       Postfix for compressed files when rewriting disabled.
+		  -h, --help          This message.
+		  -v, --version       Show version.
+		```
+		you can custom the scripts in package.json file.
+	</li>
+</ol>
+
+
+
+
+
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,204 @@
+var path = require('path');
+var webpack = require('webpack');
+
+
+/* =============================================================
+ *  config for path
+ * ============================================================ */
+var node_modules_dir = path.join(__dirname, 'node_modules');
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+
+
+/* =============================================================
+ *  plugins settings
+ * ============================================================ */
+/* minimize js,css,img... */
+var UglifyJsPlugin = new webpack.optimize.UglifyJsPlugin({
+		minimize: true,
+		compress: {
+	        warnings: false
+	    }
+	});
+
+/* html generate automatically */
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+	
+/* expose the value to global */
+var ProvidePlugin = new webpack.ProvidePlugin({
+		$: "jquery",
+	    jQuery: "jquery",
+	    "window.jQuery": "jquery"
+	});
+
+/* browserSync */
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+var BrowserSyncPlugin = new BrowserSyncPlugin({
+	host: 'localhost',
+	port: 3000,
+	server: { baseDir: ['build/'] }
+});
+
+/* common chunks */
+var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
+
+/* ExtractText */
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var CssExtractPlugin = new ExtractTextPlugin("[name].css");
+
+// merge
+var merge = require('webpack-merge');
+var TARGET = process.env.npm_lifecycle_event;
+
+
+
+
+
+/* =============================================================
+ *  base settings
+ * ============================================================ */
+var common = {
+	entry: {
+    	app: ['jquery', 'app']
+    	// app: path.resolve(ROOT_PATH, 'app/components/app/app.js')
+    },
+    output: {
+        filename: '[name].js'
+    },
+    module: {
+		loaders: [
+			//jsx
+			{
+				test: /\.jsx?$/,
+      			loader: 'babel',
+      			include: APP_PATH
+			},
+			//css
+			{
+				test: /\.css$/,
+      			loader: 'style!css',
+      			include: APP_PATH
+			},
+		    //url loader
+		    {
+		    	test: /\.(png|jpg)$/,
+		    	loader: 'url?limit=25000',
+				include: APP_PATH
+		    }
+		]
+    },
+    resolve: {
+		alias: {
+			jquery: path.resolve(ROOT_PATH, 'app/assets/js/libs/jquery/jquery.min.js'),
+			app: path.resolve(ROOT_PATH, 'app/components/app/app.js')
+		}
+    },
+    plugins: [
+    	//Common Chunk
+    	new CommonsChunkPlugin('app', 'app.js'),
+    	// new CommonsChunkPlugin('header-main.js', ['header','main']),
+    	
+		//html
+		new HtmlWebpackPlugin({
+			title: 'index page',
+			template: path.resolve(ROOT_PATH, 'app/tmpl/index.html'),
+			inject: 'head',
+			filename: 'index.html', //change it to '../index.html' if you want to test on browserSync model.
+			hash: false,
+			chunks: ['app']
+		}),
+
+
+    	ProvidePlugin
+    ]
+}
+
+/* =============================================================
+ *  dev
+ * ============================================================ */
+if(TARGET === 'dev') {
+	module.exports = merge(common, {
+    	output: {
+	        path: path.resolve(BUILD_PATH)
+	    },
+    	// devtool: "source-map",
+	    module: {
+	    	loaders: [
+				//jsx
+				{
+					test: /\.jsx?$/,
+	      			loaders: ['react-hot', 'babel'],
+	      			include: APP_PATH
+				},
+	    		//sass
+				{
+			    	test: /\.scss$/,
+			    	// loader: 'style!css?sourceMap!sass?sourceMap!autoprefixer-loader',
+			    	loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader"),
+			    	include: APP_PATH
+			    }
+	    	]
+	    },
+	    plugins: [
+			CssExtractPlugin
+    	]
+  	});
+}
+
+
+/* =============================================================
+ *  browser sync
+ * ============================================================ */
+if(TARGET === 'browsersync') {
+	module.exports = merge(common, {
+    	output: {
+	        path: path.resolve(ROOT_PATH, 'build/assets/'),
+	        publicPath: 'assets/'
+	    },
+    	// devtool: "source-map",
+	    module: {
+	    	loaders: [
+	    		//sass
+				{
+			    	test: /\.scss$/,
+			    	// loader: 'style!css?sourceMap!sass?sourceMap!autoprefixer-loader',
+			    	loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader"),
+			    	include: APP_PATH
+			    }
+	    	]
+	    },
+	    plugins: [
+			CssExtractPlugin,
+	    	BrowserSyncPlugin
+    	]
+  	});
+}
+
+
+/* =============================================================
+ *  deploy
+ * ============================================================ */
+if (TARGET === 'deploy') {
+	module.exports = merge(common, {
+    	output: {
+	        path: path.resolve(ROOT_PATH, 'dist/assets/'),
+	        publicPath: 'assets/'
+	    },
+	    module: {
+	    	loaders: [
+	    		//sass
+				{
+			    	test: /\.scss$/,
+			    	// loader: 'style!css!sass!autoprefixer-loader',
+			    	loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader"),
+			    	include: APP_PATH
+			    }
+	    	]
+	    },
+	    plugins: [
+			UglifyJsPlugin,
+			CssExtractPlugin
+    	]
+  	});
+}
