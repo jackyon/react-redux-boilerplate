08ea940729546f4ad0b497c266a1ae8bcc893706
jackyon <jackyon@jackyon1deMacBook-Pro.local>
2015-12-18 16:30:01 +0800

    update react-simple-router version

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux'
-import { routeReducer } from 'redux-simple-router'
+import { routeReducer as routing } from 'redux-simple-router'
 import example from './example.js'
 
 const rootReducer = combineReducers({
 	example,
-	routing: routeReducer
+	routing
 })
 
 export default rootReducer
