eb73da49e1ffa35edd57fd0e03f7eeae5ec119d5
jackyon <jackyon@jackyon1deMacBook-Pro.local>
2016-03-15 15:38:05 +0800

    updates

--- app/containers/About/index.js
+++ app/containers/About/index.js
@@ -5,13 +5,13 @@ import imageExample from './images/reactjs.png';
 class About extends Component {
     render() {
         const metaData = {
-          title: 'About Page',
-          description: 'About Page description',
-          canonical: 'http://example.com/path/to/page',
-          meta: {
-              charset: 'utf-8',
-              name: {
-                  keywords: 'react,meta,document,html,tags'
+            title: 'About Page',
+            description: 'About Page description',
+            canonical: 'http://example.com/path/to/page',
+            meta: {
+                charset: 'utf-8',
+                name: {
+                    keywords: 'react,meta,document,html,tags'
                 }
             }
         };
