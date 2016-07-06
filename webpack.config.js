dab4acf77843fe272d40afb023da8e197caa6782
jackyon <jackyon@jackyon1deMacBook-Pro.local>
2016-07-06 17:03:51 +0800

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
-<<<<<<< HEAD
-console.log('test');
-
-=======
-console.log('test one more time');
->>>>>>> master
+console.log('branch');
 
 /* =============================================================
  *  config for path
